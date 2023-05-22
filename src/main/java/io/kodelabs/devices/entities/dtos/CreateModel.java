b0dcd147c3dbb5af9f90b326685cc0bbacecbafd
rineseM <rinesemorina@kodelabs.com>
2023-05-22 10:18:53 +0200

    changes in pagination

--- src/main/java/io/kodelabs/devices/entities/dtos/CreateModel.java
+++ src/main/java/io/kodelabs/devices/entities/dtos/CreateModel.java
@@ -1,5 +1,6 @@
 package io.kodelabs.devices.entities.dtos;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
